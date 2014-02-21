ebbfd8cf1cb16f20ed86d6d364fee9ebc8896d9a
Kevin <ksrbkevinsuen@gmail.com>
2014-02-20 23:16:37 -0500

    function.js: cleaned up code slightly Campus Map.pdf: downloaded map for quick reference
    Really a BS commit to figure out how to use Intellij :D

--- functions.js
+++ functions.js
@@ -1,21 +1,37 @@
 function initialize() {
-	var mapOptions = {
-		center : new google.maps.LatLng(40.52349, -74.43723),
-		zoom : 17
-	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-			mapOptions);
+    var map;
+    var mapOptions;
 
-	map.set('styles', [ {
-		"featureType" : "landscape.man_made",
-		"elementType" : "geometry.fill",
-		"stylers" : [ {
-			"hue" : "#ff8800"
-		}, {
-			"saturation" : 58
-		}, {
-			"lightness" : -46
-		} ]
-	} ]);
+    map = new google.maps.Map(document.getElementById("map-canvas"));
+
+    //Many of these settings are questionable at best
+    mapOptions = {
+        center: new google.maps.LatLng(40.52349, -74.43723),
+        zoom: 17,
+        minZoom: 17,
+        panControl: false
+        // draggable: false
+    };
+
+    map.setOptions(mapOptions);
+
+    map.set('styles', [
+        {
+            "featureType": "landscape.man_made",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "hue": "#FF7A7A"
+                },
+                {
+                    "saturation": 58
+                },
+                {
+                    "lightness": -46
+                }
+            ]
+        }
+    ]);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
