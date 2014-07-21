20a4c57de7b12fe5f537df0d3fe9346b5a60d321
bschang166 <chc6@njit.edu>
2014-07-21 12:40:24 -0400

    ru.map.drawing.js is refactored and working. Not satisfied with overall flow control / structure and will refactor further

--- Rutgers_map/public/js/ru.map.drawing.js
+++ Rutgers_map/public/js/ru.map.drawing.js
@@ -1,4 +1,13 @@
-﻿'use strict';
+﻿/*
+  ru.map.drawings.js
+  Retrieves map kml data from server and draws polygon onto map
+  
+  refactor, documentation
+  
+  remove kml html jquery leftover
+*/
+
+'use strict';
 
 ru.map.drawing = (function () {
   var
@@ -8,10 +17,11 @@ ru.map.drawing = (function () {
     jqueryMap = {},
     placemarkList = [],
 
+    KmlParser = ru.util.KmlParser,
+
     setJqueryMap,
     getMapKml, parseMapKml,
-    setDrawingConfig,
-    addLatLng,
+    drawPolygonToMap,
     initModule;
 
   setJqueryMap = function () {
@@ -26,7 +36,8 @@ ru.map.drawing = (function () {
       url: '/map/kml/test'
     }).then(
       function (kmlData) {
-        parseMapKml(kmlData);
+        var parsedData = parseMapKml(null, kmlData);
+        drawPolygonToMap(parsedData, map);
       },
       function (err) {
         throw err;
@@ -34,57 +45,24 @@ ru.map.drawing = (function () {
     )
   }
 
-  parseMapKml = function (kmlData) {
+  parseMapKml = function (err,kmlData) {
+    if (err) throw err;
 
-    /*
-      need to extract:
-      <Placemark> separates each entry
-      -<name>
-      -<Polygon> <coordinates>
-    */
-    var
-      wrapper = jqueryMap.wrapper,
-      placemarks;
+    var results = new KmlParser(kmlData).parse();
+    return results;
+  }
+  //------------------- BEGIN DRAWINGS FUNCTIONS-------------------
 
-    wrapper.html(kmlData);
-
-    placemarks = wrapper.find('placemark');
-    
-    for (var i = 0, n = placemarks.length; i < n; i++) {
+  drawPolygonToMap = function (polygonEntries, map) {
+    for (var i = 0, n = polygonEntries.length; i < n; i++) {
       var
-        element, coordinates,
-        placemark = {};
+        entry, polygon;
 
-      element = placemarks.eq(i);
-      placemark.title = element.find('name').text();
-      
-      coordinates = element.find('coordinates').text().split(',0.0 ');
-
-      coordinates.forEach(function (coordset,i) {
-        var
-          coords,
-          lat, long;
-
-        coords = coordset.split(',');
-        long = parseFloat(coords[0]);
-        lat = parseFloat(coords[1]);
-
-        coordinates[i] = new google.maps.LatLng(lat, long);
-      });
-
-      placemark.coords = coordinates;
-
-      placemarkList.push(placemark);
-
-    }
-    for (var i = 0, n = placemarkList.length; i < n; i++) {
-      var
-        placemark, polygon;
-
-      placemark = placemarkList[i];
+      entry = polygonEntries[i];
 
       polygon = new google.maps.Polygon({
-        paths: placemark.coords,
+        paths: entry.coords,
+        title: entry.title,
         strokeColor: '#FF0000',
         strokeOpacity: 0.8,
         strokeWeight: 3,
@@ -93,45 +71,20 @@ ru.map.drawing = (function () {
       });
       polygon.setMap(map);
     }
-
   }
-  //------------------- BEGIN DRAWINGS CONFIG-------------------
-  polylineOptions = {
-    strokeColor: '#000000',
-    strokeOpacity: 1.0,
-    strokeWeight: 3
-  };
-  polyline = new google.maps.Polyline(polylineOptions);
-
-  setDrawingConfig = function () {
-    polyline.setMap(map);
-  }
-  //------------------- END DRAWINGS CONFIG-------------------
+  //------------------- END DRAWINGS FUNCTIONS-------------------
 
   //------------------- BEGIN EVENT HANLDERS-------------------
-  addLatLng = function (event) {
-    var
-      path, marker;
-
-    path = polyline.getPath();
-
-    path.push(event.latLng);
-
-    marker = new google.maps.Marker({
-      position: event.latLng,
-      map: map
-    });
-  }
   //------------------- END EVENT HANLDERS-------------------
 
   initModule = function () {
+    var parsedData;
+
     map = ru.map.getMap();
     setJqueryMap();
 
     getMapKml();
-    setDrawingConfig();
 
-    google.maps.event.addListener(map, 'click', addLatLng);
   }
 
   return {
---
+++ Rutgers_map/public/js/ru.util.js
@@ -0,0 +1,60 @@
+﻿/*
+  ru.util.js
+  Module for utility functions
+*/
+
+ru.util = (function () {
+
+  var
+    KmlParser;
+
+  KmlParser = function (kmlData) {
+    this.data = kmlData;
+  };
+  KmlParser.prototype.parse = function () {
+
+    var
+      hiddenWrapper = $(document.createElement('div')),
+      entries,
+      placemarkList = [];
+
+    hiddenWrapper.html(this.data);
+
+    entries = hiddenWrapper.find('placemark');
+
+    for (var i = 0, n = entries.length; i < n; i++) {
+      var
+        currentEntry, coordinates,
+        placemark = {};
+
+      currentEntry = entries.eq(i);
+      placemark.title = currentEntry.find('name').text();
+
+      coordinates = currentEntry.find('coordinates').text().split(',0.0 ');
+
+      coordinates.forEach(function (coordset, i) {
+        var
+          coords,
+          lat, long;
+
+        coords = coordset.split(',');
+        long = parseFloat(coords[0]);
+        lat = parseFloat(coords[1]);
+
+        coordinates[i] = new google.maps.LatLng(lat, long);
+      });
+
+      placemark.coords = coordinates;
+
+      placemarkList.push(placemark);
+
+    }
+    return placemarkList;
+
+  };
+
+  return {
+    KmlParser: KmlParser
+  };
+
+}());
